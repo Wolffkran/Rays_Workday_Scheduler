3bf394c275f89302f03b27b688fefa58db51ec4d
Wolffkran <liebherr2008@live.com>
2023-09-27 02:11:11 -0400

    Added code that ensures the code runs after the page is fully loaded

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+// This ensures that the code runs after the page is fully loaded.
+$(document).ready(function() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,15 +17,9 @@ $(function () {
     localStorage.setItem(timeBlockId, userInput);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   // Get the current hour using Day.js in 24-hour format (0-23)
   var currentHour = dayjs().hour();
 
-  // Loop through each time block and apply the appropriate class
   $('.time-block').each(function () {
     var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
 
@@ -40,19 +32,17 @@ $(function () {
     }
   });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  for (var i = 9; i <= 17; i++) {
-    var userTextInput = localStorage.getItem('hour-' + i);
+// Retrieve and set saved user input from localStorage for each time block
+for (var i = 9; i <= 17; i++) {
+  var userTextInput = localStorage.getItem('hour-' + i);
 
-    // Check if user input exists in localStorage for this time block
-    if (userTextInput !== null) {
-      $('#hour-' + i + ' .description').val(userTextInput);
-    }
+  // Check if user input exists in localStorage for this time block
+  if (userTextInput !== null) {
+    $('#hour-' + i + ' .description').val(userTextInput);
   }
+}
 
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current date in the header of the page
   var currentDate = dayjs().format('dddd, MMM D, YYYY [at] h:mm:ss A');
-  $('#currentDay').text(currentDate); // Display the current date in the header
+  $('#currentDay').text(currentDate); 
 });
